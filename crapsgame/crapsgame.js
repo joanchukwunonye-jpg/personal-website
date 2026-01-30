99e937479481bff21b35333c322b0b8ff4f829f9
Joan Chukwunonye <joan.chukwunonye@gmail.com>
2026-01-30 10:09:47 +0100

    Triggered dice scale format on window resize

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -106,11 +106,18 @@ function setBetAmount (betAmount) {
 
 function rollDice () {
     document.getElementById(crapsRollDiceButton).style.display = "none"
-    const diceRollElement = document.getElementById(crapsRollDiceAnimationContainer)
-    rollADie ({ element: diceRollElement, numberOfDice: 2, callback: processDiceResult, delay: 10000000});
 
+    const diceRollElement = document.getElementById(crapsRollDiceAnimationContainer)
+
+    rollADie({
+        element: diceRollElement,
+        numberOfDice: 2,
+        callback: processDiceResult,
+        delay: 10000000
+    })
 }
 
+window.addEventListener("resize", formatDiceScale);
 function formatDiceScale () {
   const vw = window.innerWidth * 0.8
   const vh = window.innerHeight * 0.8
