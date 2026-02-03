85667ee9d7aa30b16951e8cc8ef6e23295c7a7a9
Joan Chukwunonye <joan.chukwunonye@gmail.com>
2026-02-03 11:29:37 +0100

    loops test exercise play around

--- misc/hello.js
+++ misc/hello.js
@@ -1,1 +1,43 @@
-console.log("Hello I am JavaScript!")
+// Ex 1 - product by addition
+console.log('\nEXERCISE 1\n')
+let multiplicand = 12
+let multiplier = 12
+let product = 0
+
+for (let i = 0; i < multiplier; i++) {
+  product += multiplicand
+}
+
+console.log('Final product of ' + multiplicand + ' time ' + multiplier + ': ' + product)
+
+// Ex 2 - Sum of numbers from 1 to x
+
+console.log('\nEXERCISE 2\n')
+
+let stopSum = 10
+let finalSum = 0
+
+for (let i = 1; i <= stopSum; i++) {
+  finalSum += i
+}
+
+console.log('Sum of all numbers between 1 and ' + stopSum + ' is ' + finalSum)
+
+// Ex 3 - Array element of longest string
+
+console.log('\nEXERCISE 3\n')
+
+let groceryList = ["cherry", "tomato", "rasberry", "apple", "reallyLongString", "notLong"]
+let indexOfLongestString = 0
+let maxLength = 0
+
+for (let i = 0; i < groceryList.length; i++) {
+   let elementLength = groceryList[i].length
+    if (elementLength >  maxLength) {
+       indexOfLongestString = i
+       maxLength = elementLength
+    }
+}
+
+console.log(groceryList)
+console.log('Index of longest string in the array is: ' + indexOfLongestString + 'which is ' + groceryList[indexOfLongestString])
